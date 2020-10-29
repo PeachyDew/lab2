4bf2a0d33e954ce10af4a9bf17fcb7dd2e96cc66
PeachyDew <73276183+PeachyDew@users.noreply.github.com>
2020-10-29 19:36:29 +0500

    createanddelete

--- lab2.cpp
+++ lab2.cpp
@@ -1,7 +1,39 @@
 ﻿#include <iostream>
+#include <conio.h>
+using namespace std;
+
+class Point {
+public:
+    int x, y; 
+    Point() {
+        cout << "Point()\n";
+        x = 0;
+        y = 0;
+    }
+    Point(int x, int y) {
+        cout << "Point(int x, int y)\n";
+       this -> x = x;
+       this -> y = y;
+    }
+    Point(const Point &p) {
+        cout << "Point(const Point &p)\n";
+        x = p.x;
+        y = p.y;
+    }
+    ~Point() {
+        cout << "~Point()\n";
+        cout << x << ", " << y << endl;
+   }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    {
+        Point p;
+        Point p2(10, 20);
+        Point p3(p2);
+    }
+    _getch();
+
 }
 
