73a31d36ec1108cfac9b0e763cd1f9b348d32794
PeachyDew <73276183+PeachyDew@users.noreply.github.com>
2020-10-30 02:04:37 +0500

    coloredpoint

--- lab2.cpp
+++ lab2.cpp
@@ -3,8 +3,9 @@
 using namespace std;
 
 class Point {
-public:
+protected:
     int x, y; 
+public:
     Point() {
         cout << "Point()\n";
         x = 0;
@@ -24,15 +25,65 @@ public:
         cout << "~Point()\n";
         cout << x << ", " << y << endl;
    }
+    void move(int dx, int dy) {
+        x = x + dx;
+        y = y + dy;
+    }
+    void reset();
 };
+
+class ColoredPoint: Point {
+protected:
+    int color;
+public:
+    ColoredPoint():Point() {
+        cout << "ColoredPoint()\n";
+        color = 0;
+    }
+    ColoredPoint(int x, int y, int color):Point(x,y) {
+        cout << "ColoredPoint(int x, int y)\n";
+        this->color = color;
+    }
+    ColoredPoint(const ColoredPoint& p) {
+        cout << "ColoredPoint(const Point &p)\n";
+        color = p.color;
+        x = p.x;
+        y = p.y;
+    }
+    ~ColoredPoint() {
+        cout << "~ColoredPoint()\n";
+        cout << x << ", " << y << endl;
+    }
+    void change_color(int new_color) {
+        color = new_color;
+    }
+   
+};
+
+
 
 int main()
 {
     {
-        Point p;
-        Point p2(10, 20);
-        Point p3(p2);
+       // Point p;
+        //Point p2(10, 20);
+        //Point p3(p2);
     }
+    
+    //Point* p4 = new Point;
+    Point* p5 = new Point(10, 20);
+    //Point* p6 = new Point(*p5);
+
+    p5->reset();
+    p5->move(10, 10);
+   
+   // delete p4;
+    delete p5;
+   // delete p6;
+    
+    ColoredPoint* cp = new ColoredPoint(1, 2, 42);
+    delete cp;
+
     _getch();
 
 }
