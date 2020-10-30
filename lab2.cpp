8a2821c73a853757e484daa6c7c5d7ac40de191c
PeachyDew <73276183+PeachyDew@users.noreply.github.com>
2020-10-30 18:19:53 +0500

    Section

--- lab2.cpp
+++ lab2.cpp
@@ -29,10 +29,12 @@ public:
         x = x + dx;
         y = y + dy;
     }
-    void reset();
+   // void reset();
 };
 
-class ColoredPoint: Point {
+
+
+class ColoredPoint: public Point {
 protected:
     int color;
 public:
@@ -41,18 +43,18 @@ public:
         color = 0;
     }
     ColoredPoint(int x, int y, int color):Point(x,y) {
-        cout << "ColoredPoint(int x, int y)\n";
+        cout << "ColoredPoint(int x, int y, int color)\n";
         this->color = color;
     }
     ColoredPoint(const ColoredPoint& p) {
-        cout << "ColoredPoint(const Point &p)\n";
+        cout << "ColoredPoint(const ColoredPoint &p)\n";
         color = p.color;
         x = p.x;
         y = p.y;
     }
     ~ColoredPoint() {
-        cout << "~ColoredPoint()\n";
         cout << x << ", " << y << endl;
+        cout << "~ColoredPoint()\n";
     }
     void change_color(int new_color) {
         color = new_color;
@@ -60,30 +62,63 @@ public:
    
 };
 
-
+class Section {
+protected:
+    Point* p1;
+    Point* p2;
+public:
+    Section() {
+        cout << "Section()\n";
+        p1 = new Point;
+        p2 = new Point;
+    }
+    Section(int x1, int y1, int x2, int y2) {
+        cout << "Section(int x1, int y1, int x2, int y2)\n";
+        p1 = new Point(x1,y1);
+        p2 = new Point(x2,y2);
+    }
+    Section(const Section& s) {
+        cout << "Section(const Section &s)\n";
+        p1 = new Point(*(s.p1));
+        p2 = new Point(*(s.p2));
+    }
+    ~Section() {
+        //cout << x << ", " << y << endl;
+        delete p1;
+        delete p2;
+        cout << "~Section()\n";
+    }
+};
 
 int main()
 {
     {
-       // Point p;
-        //Point p2(10, 20);
-        //Point p3(p2);
+        /*Point p;
+        Point p2(10, 20);
+        Point p3(p2);*/
     }
     
     //Point* p4 = new Point;
     Point* p5 = new Point(10, 20);
     //Point* p6 = new Point(*p5);
 
-    p5->reset();
+    //p5->reset();
     p5->move(10, 10);
    
-   // delete p4;
+   //delete p4;
     delete p5;
-   // delete p6;
+   //delete p6;
     
     ColoredPoint* cp = new ColoredPoint(1, 2, 42);
+    Point* p7 = new ColoredPoint(1, 2, 42);
     delete cp;
+    delete p7;
 
+    Section* s1 = new Section;
+    Section* s2 = new Section(*s1);
+
+    delete s1;
+    delete s2;
     _getch();
 
 }
